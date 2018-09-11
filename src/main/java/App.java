e39097c32a3cbd7967f40f404b969323ea8b9594
vaddi <sudhakarvaddi11@gmail.com>
2018-09-11 11:42:02 +0530

    Added WEBHOOK 2 in Java prg

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World I am sending a WEBHOOK");
+        System.out.println("Hello, World I am sending a WEBHOOK 2nd time");
     }
 }
